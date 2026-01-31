2e1208b7ad3d16c23cb3160afbd168fca6e47194
Alexiboi <alexhanna413@gmail.com>
2026-01-31 23:01:01 +0000

    Add Card, Game, Player classes along with Rank and Suit enums

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+
+    public Game(){
+
+    }
+
+    public void run_game(int no_players){
+
+    }
+
+    public String print_game(){
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Player {
+
+    private String name;
+    private List<Card> hand;
+
+    public Player(){}
+}
